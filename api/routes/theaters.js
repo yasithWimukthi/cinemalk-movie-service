e68aed71af6e06a7957fd1f17cce09b570b9fb71
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-14 18:36:49 +0530

    Delete theater API

--- api/routes/theaters.js
+++ api/routes/theaters.js
@@ -81,4 +81,22 @@ router.put("/:theaterID", (req, res) => {
     });
 });
 
+// theaters: delete theater
+router.delete("/:theaterID", (req, res) => {
+    const id = req.params.theaterID;
+    //delete theater
+    const deletedTheater = Theater.findByIdAndDelete(id);
+    deletedTheater
+      .then((result) => {
+        res.status(200).json({
+          message: "DELETE request",
+          deletedTheater: result,
+        });
+      })
+      .catch((err) => {
+        res.status(500).json({
+          err: err.message,
+        });
+      });
+  });
 module.exports = router;
