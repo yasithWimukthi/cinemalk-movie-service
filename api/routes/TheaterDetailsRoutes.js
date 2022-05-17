6e3a262a98b176c7474e888869467067b67e7022
yasith <it19966922@my.sliit.lk>
2022-05-17 11:25:15 +0530

    create get all method

--- api/routes/TheaterDetailsRoutes.js
+++ api/routes/TheaterDetailsRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const TheaterDetails = require("../controllers/TheaterDetailsController");
 
+router.get('/', TheaterDetails.getAll);
 router.post('/addTheater',TheaterDetails.addMovieToTheater)
 
 module.exports = router;
