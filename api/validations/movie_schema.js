34b2baa81513c5403e4814b8367f9b154680bff6
MadhaviImashi <madhaviimashi@gmail.com>
2022-05-18 23:56:47 +0530

    mistake corrected

--- api/validations/movie_schema.js
+++ api/validations/movie_schema.js
@@ -4,7 +4,7 @@ const schema = Joi.object({
     title: Joi.string().required(),
     released_date:  Joi.string().required(),
     overview: Joi.string().required(),
-    genre: Joi.string().required()
+    genres: Joi.string().required()
 
 })
 module.exports = schema;
